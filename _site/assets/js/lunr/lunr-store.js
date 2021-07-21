f03a9ad23839e5bb8e0470603c2b8f08c3257d34
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-07-21 18:12:59 +0900

    fix blog setting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "부의 추월차선 unscripted",
-        "excerpt":"부의 추월차선 내용 정리  ","categories": ["독서"],
+        "excerpt":"1. 최신 기술 공부하기   하이하이   2. 최신 뉴스 게시하기   하이하이   3. 토이 프로젝트 소개하기   하이하이  ","categories": ["독서"],
         "tags": [],
         "url": "/%EB%8F%85%EC%84%9C/unscripted/",
         "teaser": null
