686aa62959fed8d5e8fef4c5e5465d0ebeab5c48
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-07-25 00:47:51 +0900

    css update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/%EB%8F%85%EC%84%9C/unscripted/",
         "teaser": null
+      },{
+        "title": "노마드코드 - 카카오톡 클론 코딩",
+        "excerpt":"Inline Inline은 height와 width를 가질수 없음. Margin(box의 border로 부터 바깥에 있는 공간)은 좌우만 적용됨. Block Block을 Inline으로 바꿀려면 div { display: Inline; } 반대는 div { display: block; } Inline이면서, block으로 사용하기 위해서는 div { display: Inline-block; } width와 height를 가질수 있으면서 옆에 다른 element가 올 수 있지만, Inline-block은 Suck!!...","categories": ["CSS"],
+        "tags": [],
+        "url": "/css/",
+        "teaser": null
       }]
