2967760a35d2e6fc8e71191723643c32cff41858
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-08-01 17:21:09 +0900

    2021-08-01

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "/css/",
         "teaser": null
+      },{
+        "title": "Today I do (2021-08-01)",
+        "excerpt":"바닐라 JS 강의(#3.0~#3.2) Javascript로 정보를 가지고 올 수 있는 방법은 document 객체와 element를 가져오는 함수를 이용 하는 것. const title = document.getElementById(\"title\"); title.innerText = \"Got you\"; getElementById 함수를 통해 id로 element를 가져올수 있다. console.dir()을 이용하면 객체의 속성을 계층 구조로 출력 할 수 있다. 투자 동박의 HS code는 7410.11 일진머티리얼의 본사는...","categories": ["Today I do"],
+        "tags": [],
+        "url": "/today%20i%20do/",
+        "teaser": null
       }]
