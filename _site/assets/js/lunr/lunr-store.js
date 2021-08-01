8eb358ab71d0e3a74c61047761f2d758aac4f062
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-08-01 20:53:07 +0900

    2021-08-01

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,18 +1,6 @@
 var store = [{
-        "title": "부의 추월차선 unscripted",
-        "excerpt":"1. 최신 기술 공부하기   하이하이   2. 최신 뉴스 게시하기   하이하이   3. 토이 프로젝트 소개하기   하이하이  ","categories": ["독서"],
-        "tags": [],
-        "url": "/%EB%8F%85%EC%84%9C/unscripted/",
-        "teaser": null
-      },{
-        "title": "노마드코드 - 카카오톡 클론 코딩",
-        "excerpt":"Inline Inline은 height와 width를 가질수 없음. Margin(box의 border로 부터 바깥에 있는 공간)은 좌우만 적용됨. Block Block을 Inline으로 바꿀려면 div { display: Inline; } 반대는 div { display: block; } Inline이면서, block으로 사용하기 위해서는 div { display: Inline-block; } width와 height를 가질수 있으면서 옆에 다른 element가 올 수 있지만, Inline-block은 Suck!!...","categories": ["CSS"],
-        "tags": [],
-        "url": "/css/",
-        "teaser": null
-      },{
         "title": "Today I do (2021-08-01)",
-        "excerpt":"바닐라 JS 강의(#3.0~#3.2) Javascript로 정보를 가지고 올 수 있는 방법은 document 객체와 element를 가져오는 함수를 이용 하는 것. const title = document.getElementById(\"title\"); title.innerText = \"Got you\"; getElementById 함수를 통해 id로 element를 가져올수 있다. console.dir()을 이용하면 객체의 속성을 계층 구조로 출력 할 수 있다. 투자 동박의 HS code는 7410.11 일진머티리얼의 본사는...","categories": ["Today I do"],
+        "excerpt":"바닐라 JS 강의(#3.0~#3.2). Javascript로 정보를 가지고 올 수 있는 방법은 document 객체와 element를 가져오는 함수를 이용 하는 것. const title = document.getElementById(\"title\"); title.innerText = \"Got you\"; getElementById 함수를 통해 id로 element를 가져올수 있다. console.dir()을 이용하면 객체의 속성을 계층 구조로 출력 할 수 있다. 투자 공부. 동박의 HS code는 7410.11으로 추측됨....","categories": ["Today I do"],
         "tags": [],
         "url": "/today%20i%20do/",
         "teaser": null
